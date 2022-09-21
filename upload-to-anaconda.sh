76aae4aab0bed88b7beb5a065c07e29d40e47892
Abdullah Diab <mpcabd@gmail.com>
2022-09-21 23:43:44 +0100

    Updated conda files

--- upload-to-anaconda.sh
+++ upload-to-anaconda.sh
@@ -3,13 +3,13 @@
 # conda config --set anaconda_upload no
 # conda config --add channels conda-forge
 # conda-build . --python 3.6
-conda-build . --python 3.7
-conda-build . --python 3.8
-conda-build . --python 3.9
-conda-build . --python 3.10
+# conda-build . --python 3.7
+# conda-build . --python 3.8
+# conda-build . --python 3.9
+# conda-build . --python 3.10
 # anaconda login
 # anaconda upload --user mpcabd /opt/conda/conda-bld/noarch/arabic-reshaper-*-py36_0.tar.bz2
-anaconda upload --user mpcabd /opt/conda/conda-bld/noarch/arabic-reshaper-*-py37_0.tar.bz2
-anaconda upload --user mpcabd /opt/conda/conda-bld/noarch/arabic-reshaper-*-py38_0.tar.bz2
-anaconda upload --user mpcabd /opt/conda/conda-bld/noarch/arabic-reshaper-*-py39_0.tar.bz2
-anaconda upload --user mpcabd /opt/conda/conda-bld/noarch/arabic-reshaper-*-py310_0.tar.bz2
+# anaconda upload --user mpcabd /opt/conda/conda-bld/noarch/arabic-reshaper-*-py37_0.tar.bz2
+# anaconda upload --user mpcabd /opt/conda/conda-bld/noarch/arabic-reshaper-*-py38_0.tar.bz2
+# anaconda upload --user mpcabd /opt/conda/conda-bld/noarch/arabic-reshaper-*-py39_0.tar.bz2
+# anaconda upload --user mpcabd /opt/conda/conda-bld/noarch/arabic-reshaper-*-py310_0.tar.bz2
