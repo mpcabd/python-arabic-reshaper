dab384eedb7fd317ce46ccafb82bfa3462f6bac4
Abdullah Diab <mpcabd@gmail.com>
2022-09-21 21:37:21 +0100

    Add py10 to testing

--- upload-to-anaconda.sh
+++ upload-to-anaconda.sh
@@ -6,8 +6,10 @@
 conda-build . --python 3.7
 conda-build . --python 3.8
 conda-build . --python 3.9
+conda-build . --python 3.10
 # anaconda login
 # anaconda upload --user mpcabd /opt/conda/conda-bld/noarch/arabic-reshaper-*-py36_0.tar.bz2
 anaconda upload --user mpcabd /opt/conda/conda-bld/noarch/arabic-reshaper-*-py37_0.tar.bz2
 anaconda upload --user mpcabd /opt/conda/conda-bld/noarch/arabic-reshaper-*-py38_0.tar.bz2
 anaconda upload --user mpcabd /opt/conda/conda-bld/noarch/arabic-reshaper-*-py39_0.tar.bz2
+anaconda upload --user mpcabd /opt/conda/conda-bld/noarch/arabic-reshaper-*-py310_0.tar.bz2
